1c86cb244e5421b6ef2d7bdaa770efc6f97b0633
krasi <krasimir.xristov@gmail.com>
2026-05-18 17:43:22 +0200

    feat: simplify CTAs markup in HeroSection component

--- features/home/components/HeroSection.tsx
+++ features/home/components/HeroSection.tsx
@@ -63,10 +63,7 @@ const HeroSection = () => {
           </m.p>
 
           {/* CTAs */}
-          <m.div
-            variants={itemVariants}
-            className='flex flex-wrap gap-4 mt-2'
-          >
+          <m.div variants={itemVariants} className='flex flex-wrap gap-4 mt-2'>
             <Link
               href='/projects'
               className='bg-[#06b6d4] text-[#09090b] px-8 py-3 font-bold rounded-lg hover:brightness-110 shadow-[0_0_20px_rgba(6,182,212,0.25)] transition-all flex items-center gap-2 text-sm'
